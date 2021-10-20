47663ecfe05e43a674a4afd051a47356999b7712
JustNekr <justnekr@gmail.com>
2021-10-20 18:13:12 +0200

    lesson_7

--- lesson_7/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-x = 'https://res.cloudinary.com/lmru/image/upload/f_auto,q_auto,w_82,h_82,c_pad,b_white,d_photoiscoming.png/LMCode/18868588.jpg'
-
-y = re.sub('_\d+', '_400', x)
-print(x)
-print(y)
